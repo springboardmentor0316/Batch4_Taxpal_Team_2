b36a3735b209df58abbcab6f28c3bc0419281316
vamshi8977 <ramanagari.vamshi.13102004@gmail.com>
2025-10-31 19:26:58 +0530

    setting

--- taxpal-dashboard/src/App.jsx
+++ taxpal-dashboard/src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import './App.css'
-import Sidebar from "./Sidebar";
-import Home from "./Home";
+import Sidebar from "./sidebar";
+import Home from "./home";
 import Setting from"./setting";
 
 export default function App() {
@@ -13,7 +13,7 @@ export default function App() {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/setting" element={<Setting />} />
-            <Route path="*" element={<div style={{padding:20}}>Page not found</div>} />
+            {/* <Route path="*" element={<div style={{padding:20}}>Page not found</div>} /> */}
         </Routes>
       </main>
     </div>
